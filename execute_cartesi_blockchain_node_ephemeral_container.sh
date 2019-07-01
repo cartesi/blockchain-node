dcbd1cfc05faefd402baee9380022157d89cc4d0
Carlo Fragni <carlo@cartesi.io>
2019-07-01 17:32:39 -0300

    Change listening ip of ephemeral container
    So it can be accessed by other containers. There is a special way docker treats localhost bindings that "isolate" containers so changing the listening ip to all available makes the ganache server available to other containers throught other IPs of the host

--- execute_cartesi_blockchain_node_ephemeral_container.sh
+++ execute_cartesi_blockchain_node_ephemeral_container.sh
@@ -1,2 +1,2 @@
 mkdir -p exported-node-files
-docker run -it --name ephemeral-cartesi-blockchain-node -p 127.0.0.1:8545:8545 -v `pwd`/exported-node-files:/root/host --rm cartesi/image-blockchain-node
+docker run -it --name ephemeral-cartesi-blockchain-node -p 0.0.0.0:8545:8545 -v `pwd`/exported-node-files:/root/host --rm cartesi/image-blockchain-node
